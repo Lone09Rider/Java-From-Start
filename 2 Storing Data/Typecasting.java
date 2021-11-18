f867edb6aed698fb7d119ce0ecb38c17c5c0fccc
Lone09Rider <85149949+Lone09Rider@users.noreply.github.com>
2021-11-18 09:58:35 +0530

    Add files via upload

---
+++ 2 Storing Data/Typecasting.java
@@ -0,0 +1,19 @@
+
+public class Typecasting {
+	
+	public static void main(String[] args) {
+	int a = 67;
+	char c = (char)(a);
+	System.out.println(c);
+	
+	float f = (float)a;
+	System.out.println(f);
+	
+	float fl = 10.7f;
+	System.out.println(fl);
+	
+	double d = (double)(a);
+	System.out.println(d);
+
+	}
+}
